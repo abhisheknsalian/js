06eceb55b4c9e772d41934c9f07f6dda93fc1ad5
abhisheknsalian <113782138+abhisheknsalian@users.noreply.github.com>
2025-01-19 03:52:35 +0000

    commit

---
+++ variables.js
@@ -0,0 +1,6 @@
+const accountID=1234
+let accountEmail="abhishek@google.com"
+var accountPassword=123
+Place="Bengaluru"
+var system
+console.table([accountID,accountEmail,accountPassword,Place,system])
